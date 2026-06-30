5216d7a59bcba7c7ea6f7e9d5dba7427f1d3bc8c
nunezbeautysupply-dev <nunezbeautysupply@gmail.com>
2026-06-29 21:49:37 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,44 @@
-var CACHE="nbs-v1";
-self.addEventListener("install",function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){
-    return c.addAll(["./","./index.html"]);
-  }));
+var CACHE_NAME = 'nbs-cache-v1';
+var FILES_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
-self.addEventListener("fetch",function(e){
-  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(name){
+          return name !== CACHE_NAME;
+        }).map(function(name){
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request).then(function(fetchResponse){
+        return caches.open(CACHE_NAME).then(function(cache){
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      }).catch(function(){
+        return caches.match('./index.html');
+      });
+    })
+  );
 });
